ecbb82655606c2c7da9162563018aeb474f04e32
Angela de Araujo <angela.araujo.rj@gmail.com>
2023-09-04 21:30:40 -0300

    Primeiro commit

---
+++ src/test/java/com/accenture/academico/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.academico.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
