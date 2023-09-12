bd4d1f435b950166d82d87bbb9ed656b11722f6b
Andre Thome <andrelthome@gmail.com>
2023-09-12 17:34:05 -0300

    Add Agencia

---
+++ bin/README.md
@@ -0,0 +1,16 @@
+# Sistema Bancário
+
+Projeto de conclusão do curso de Java Avançado.
+
+## Integrantes
+
+- André Thomé
+- Ângela de Araújo
+- Catarina Almeida
+
+## Tecnologia
+
+- Java 17
+- Spring Boot
+- Maven
+- MySQL
---
+++ src/main/java/com/accenture/academico/bank/model/Agencia.java
@@ -0,0 +1,117 @@
+package com.accenture.academico.bank.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Agencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(nullable = false, length = 4)
+    private String numAgencia;
+
+    public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNumAgencia() {
+		return numAgencia;
+	}
+
+	public void setNumAgencia(String numAgencia) {
+		this.numAgencia = numAgencia;
+	}
+
+	public String getNomeAgencia() {
+		return nomeAgencia;
+	}
+
+	public void setNomeAgencia(String nomeAgencia) {
+		this.nomeAgencia = nomeAgencia;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Column(nullable = false, length = 255)
+    private String nomeAgencia;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id", nullable = false)
+    private Endereco endereco;
+
+    @Column(nullable = false, length = 16)
+    private String telefone;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((numAgencia == null) ? 0 : numAgencia.hashCode());
+        result = prime * result + ((nomeAgencia == null) ? 0 : nomeAgencia.hashCode());
+        result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+        result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Agencia other = (Agencia) obj;
+        if (id != other.id)
+            return false;
+        if (nomeAgencia == null) {
+            if (other.nomeAgencia != null)
+                return false;
+        } else if (!nomeAgencia.equals(other.nomeAgencia))
+            return false;
+        if (endereco == null) {
+            if (other.endereco != null)
+                return false;
+        } else if (!endereco.equals(other.endereco))
+            return false;
+        if (telefone == null) {
+            if (other.telefone != null)
+                return false;
+        } else if (!telefone.equals(other.telefone))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Agencia [Agencia=" + numAgencia + ", nome=" + nomeAgencia + ", telefone="  + ", endereco=" + endereco+ telefone + "]";
+    }
+
+}
